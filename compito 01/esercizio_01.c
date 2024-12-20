f1d948977e7b517b35c10579b032c63b2e1d5c86
Distiere <bugman3652@gmail.com>
2024-12-20 16:59:05 +0000

    esercizio finito

---
+++ compito 01/esercizio_01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char nome [50];
+    char cognome [50];
+    printf("Inserisci il tuo nome: ");
+    scanf("%s", nome);
+    printf("Inserisci il tuo cognome: ");
+    scanf("%s", cognome);
+    printf("Benvenuto, %s %s\n", nome, cognome);
+    printf("Grazie per aver usato il nostro programma.\nTi auguriamo Buone feste\n   *\n  ***\n *****\n*******\n  ***\n\n");
+
+
+    return 0;
+
+    
+}
