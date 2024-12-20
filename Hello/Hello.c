19e48a73c7b67f912f48aa28a21ef40f3964e89f
Distiere <bugman3652@gmail.com>
2024-12-20 16:09:17 +0000

    hello. c updated

--- Hello/Hello.c
+++ Hello/Hello.c
@@ -6,6 +6,18 @@
 int main()
 {
     printf("Hello World!!!\n\n\n");
+    
+    //variable of 32 chars
+    char name [32];
+
+    // ask for a name
+    printf("enter a name: ");
+
+    //read the input
+    scanf("%s", name);
+
+    //print a message wiht name entered
+    printf("\nHello\t%s\n\n\n", name);
 
     return 0;
 }
